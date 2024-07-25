7e0b5aa5f21af75b5302c383b14d5c1862c07f72
Ester Stankovska <stankovskaester@gmail.com>
2024-07-25 17:57:16 +0200

    exs.

---
+++ loops/reverse.js
@@ -0,0 +1,6 @@
+// Create a loop that will display in the console all numbers from 10 to 1 (in the reverse order).
+
+
+for (let i = 10; i <=0; i--) {
+console.log(i)
+}
--- projects/coffee_machine/coffeeTwo.js
+++ projects/coffee_machine/coffeeTwo.js
@@ -1,4 +1,4 @@
-const input = require('sync-input')
+const input = require('sync-input');
 
 let waterInMachine = 400;
 let milkInMachine = 540;
@@ -6,43 +6,63 @@ let beansInMachine = 120;
 let cupsAvailable = 9;
 let moneyInMachine = 550;
 
-console.log(`The coffee machine has:
+
+let shopOpen = true;
+
+while(shopOpen) {
+    console.log(`The coffee machine has:
 ${waterInMachine} ml of water
 ${milkInMachine} ml of milk
 ${beansInMachine} g of coffee beans
 ${cupsAvailable} disposable cups
-${moneyInMachine} of money`)
+${moneyInMachine} of money`);
 
-  console.log("Write action (buy, fill, take): ")
-
-
-  let choice = input();
+    console.log("Write action (buy, fill, take): ");
 
 
-  if(choice === "buy") {
-    console.log("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
-    let coffeeChoice = input();
+    let choice = input();
 
-    if(coffeeChoice === "1") {
-      waterInMachine - 250;
-      beansInMachine - 16;
-     milkInMachine + 4;
-      cupsAvailable --;
+    if (choice === "buy") {
+        console.log("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
+        let coffeeChoice = input();
+
+        if (coffeeChoice === "1") {
+            waterInMachine -= 250;
+            beansInMachine -= 16;
+            moneyInMachine += 4;
+            cupsAvailable--;
+        } else if (coffeeChoice === "2") {
+            waterInMachine -= 350;
+            milkInMachine -= 75;
+            beansInMachine -= 20;
+            moneyInMachine += 7;
+            cupsAvailable--;
+        } else {
+            waterInMachine -= 200;
+            milkInMachine -= 100;
+            beansInMachine -= 12;
+            moneyInMachine += 6;
+            cupsAvailable--;
+        }
     }
-    else if(coffeeChoice == "2") {
-     waterInMachine - 350;
-      milkInMachine - 75;
-      beansInMachine - 20;
-     milkInMachine + 7;
-     cupsAvailable --;
-   }
-    else {
-     waterInMachine - 200;
-      milkInMachine - 100;
-     beansInMachine - 12;
-     milkInMachine + 6;
-     cupsAvailable --;
-   }
-  }
+    else if (choice === "fill") {
+        console.log("Write how many ml of water you want to add: ");
+        let addWater = parseInt(input(), 10);
+        waterInMachine += addWater;
+        console.log("Write how many ml of milk you want to add: ");
+        let addMilk = parseInt(input(), 10);
+        milkInMachine += addMilk;
+        console.log("Write how many grams of coffee beans you want to add: ");
+        let addBeans = parseInt(input(), 10);
+        beansInMachine += addBeans;
+        console.log("Write how many disposable cups you want to add: ");
+        let addCups = parseInt(input(), 10);
+        cupsAvailable += addCups;
+    }
+
+    else if (choice === "take") {
+        moneyInMachine = 0;
+    }
+}
 
 
